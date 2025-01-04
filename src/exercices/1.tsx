4a683b4a16541db64b14fbf866dea242a7034e00
khuq <ugur_hakan@yahoo.fr>
2025-01-04 02:12:57 +0100

    Ex 1: Start tik tak toe

--- src/exercices/1.tsx
+++ src/exercices/1.tsx
@@ -1,29 +1,83 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
 import clsx from "clsx";
+import { ComponentPropsWithoutRef } from "react";
+import { calculateNextValue, calculateStatus } from "../lib/tictactoe/helpers";
 
 // 🦁 Supprime ce commentaire et définis correctement les types pour ce composant
-type SquareProps = any;
+type SquareProps = {
+  isWinningSquare?: boolean;
+} & ComponentPropsWithoutRef<"button">;
 
-const Square = (props: SquareProps) => {
+const Square = ({ isWinningSquare, children, ...props }: SquareProps) => {
   // 🦁 Remplace ça par les props définies en haut
   return (
     <button
       className={clsx("square", {
-        "winning-square": false, // 🦁 Remplace ça par la prop isWinningSquare
+        "winning-square": isWinningSquare, // 🦁 Remplace ça par la prop isWinningSquare
       })}
+      {...props}
     >
-      O {/* 🦁 Remplace ça par la prop children */}
+      {children} {/* 🦁 Remplace ça par la prop children */}
     </button>
   );
 };
 
+type squareValue = "O" | "X" | null;
+
+type BoardProps = {
+  // ...
+  squares: squareValue[];
+  winningSquares?: number[];
+  onClick?: (index: number) => void;
+};
+const Board = ({ squares, onClick, winningSquares }: BoardProps) => {
+  return (
+    <div className="game-board">
+      {squares.map((square, i) => (
+        <Square
+          onClick={() => onClick?.(i)}
+          key={i}
+          isWinningSquare={winningSquares?.includes(i)}
+        >
+          {square}
+        </Square>
+      ))}
+    </div>
+  );
+};
+
+const getDefaultSquares = (): SquareValue[] => [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  "O",
+  null,
+  "X",
+];
+
+type GameInfoProps = { status: string };
+
+const GameInfo = ({ status, userNames }: GameInfoProps) => {
+  return (
+    <div className="game-info">
+      <div>{status}</div>
+    </div>
+  );
+};
+
 const Game = () => {
+  const squares = getDefaultSquares();
+  const nextPlayer = calculateNextValue(squares);
+  const status = calculateStatus(squares, nextPlayer);
+
   return (
     <div className="game">
-      <Square isWinningSquare={true}>X</Square>
-      <Square isWinningSquare={false}>X</Square>
-      <Square isWinningSquare={true}>O</Square>
+      <GameInfo status={status} />
+      <Board squares={squares} winningSquares={[0, 1, 2, 3]} />
     </div>
   );
 };
